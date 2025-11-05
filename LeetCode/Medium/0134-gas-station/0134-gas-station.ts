c60874b10e8e9e6e521fc50f3b9dcbf4b86674ec
Candies <50367342+Wichen78@users.noreply.github.com>
2025-11-05 13:45:10 +0100

    Time: 1 ms (89.38%), Space: 66.2 MB (76.21%) - LeetHub

--- LeetCode/Medium/0134-gas-station/0134-gas-station.ts
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.ts
@@ -1,24 +1,17 @@
 function canCompleteCircuit(gas: number[], cost: number[]): number {
-    const gastank = gas.map((val, index) => val - cost[index]);
-    if (gastank.reduce((a, b) => a + b, 0) < 0) {
-        return -1;
-    }
-    for (let i = 0, tank = 0, useReserveIndex = -1; i < gastank.length; i++, tank = 0, useReserveIndex = -1) {
-        for (let j = 0; j < gastank.length; j++) {
-            if (useReserveIndex === -1 && gastank[(i + j) % gastank.length] < 0) {
-                useReserveIndex = (i + j) % gastank.length;
-            }
-            tank += gastank[(i + j) % gastank.length];
-            if (tank < 0) {
-                if (useReserveIndex !== -1) {
-                    i = useReserveIndex;
-                }
-                break;
-            }
-        }
-        if (tank >= 0) {
-            return i;
+    let total = 0;
+    let current = 0;
+    let start = 0;
+
+    for (let i = 0; i < gas.length; i++) {
+        total += (gas[i] - cost[i]);
+        current += (gas[i] - cost[i]);
+
+        if (current < 0) {
+            start = i + 1;
+            current = 0
         }
     }
-    return -1;
+
+    return total >= 0 ? start : -1;
 };
