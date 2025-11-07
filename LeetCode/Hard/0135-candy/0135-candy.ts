42db66fcf319e636aa3750906f91f877d642360f
Candies <50367342+Wichen78@users.noreply.github.com>
2025-11-07 12:33:12 +0100

    Time: 6 ms (64.62%), Space: 59.8 MB (65.23%) - LeetHub

---
+++ LeetCode/Hard/0135-candy/0135-candy.ts
@@ -0,0 +1,17 @@
+function candy(ratings: number[]): number {
+    const len = ratings.length;
+    const res = new Array(len).fill(1);
+    for (let i = 1; i < len; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            if (res[i] <= res[i - 1]) {
+                res[i] = res[i - 1] + 1;
+            }
+        }
+        if (ratings[len - i - 1] > ratings[len - i]) {
+            if (res[len - i - 1] <= res[len - i]) {
+                res[len - i - 1] = res[len - i] + 1;
+            }
+        }
+    }
+    return res.reduce((a, b) => a + b, 0);
+};
