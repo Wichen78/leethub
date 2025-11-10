7e79f0b6e97d3dad27dea73f202c8d646aaa0f63
Candies <50367342+Wichen78@users.noreply.github.com>
2025-11-10 14:05:18 +0100

    Time: 2635 ms (5.01%), Space: 57.6 MB (56.43%) - LeetHub

---
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.ts
@@ -0,0 +1,28 @@
+function trap(height: number[]): number {
+    let water = 0;
+    let i = 0;
+    while (i < height.length) {
+        if (height[i] > 0 || i < height.length - 1 && height[i] > height[i + 1]) {
+            let nextStep = i + 1;
+            for (let h = 1, j = 1; h <= height[i] && i + j < height.length; h++, j = 1) {
+                while (i + j < height.length && height[i + j] < h) {
+                    j++;
+                }
+                if (i + j >= height.length) {
+                    continue;
+                } else if (j === 1) {
+                    h = height[i + j];
+                } else {
+                    water += j - 1;
+                }
+                if (h === 1) {
+                    nextStep = i + j;
+                }
+            }
+            i = nextStep;
+        } else {
+            i++;
+        }
+    }
+    return water;
+};
