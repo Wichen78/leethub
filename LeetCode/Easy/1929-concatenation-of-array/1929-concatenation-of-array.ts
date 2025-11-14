6267f32b80597edc0afc13d6160dec15f4ad4179
Candies <50367342+Wichen78@users.noreply.github.com>
2025-11-14 10:51:48 +0100

    Time: 1 ms (60.28%), Space: 61.1 MB (8.06%) - LeetHub

--- LeetCode/Easy/1929-concatenation-of-array/1929-concatenation-of-array.ts
+++ LeetCode/Easy/1929-concatenation-of-array/1929-concatenation-of-array.ts
@@ -1,9 +1,3 @@
 function getConcatenation(nums: number[]): number[] {
-    const n = nums.length;
-    const ans = new Array(n);
-    for (let i = 0; i < n; i++) {
-        ans[i] = nums[i];
-        ans[i + n] = nums[i];
-    }
-    return ans;
+    return [...nums, ...nums];
 };
