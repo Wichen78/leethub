13da3fea57580a5cb990f0cf28491ae7c51f4dcf
Candies <50367342+Wichen78@users.noreply.github.com>
2025-11-17 13:32:38 +0100

    Time: 1 ms (64.08%), Space: 55.6 MB (78.61%) - LeetHub

--- LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.ts
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -1,12 +1,12 @@
 function longestCommonPrefix(strs: string[]): string {
     const sorted = strs.sort();
     const minLength = sorted.reduce((acc, cur) => cur.length < acc ? cur.length : acc, sorted[0].length);
-    const first = sorted[0].slice(0, minLength);
-    const last = sorted[sorted.length - 1].slice(0, minLength);
+    const first = sorted[0];
+    const last = sorted[sorted.length - 1];
     for (let i = 0; i < minLength; i++) {
         if (first[i] !== last[i]) {
             return first.slice(0, i);
         }
     }
-    return first;
+    return first.slice(0, minLength);
 };
