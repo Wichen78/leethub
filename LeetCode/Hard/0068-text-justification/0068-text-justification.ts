df211d4ea63b83ce59d1185ee6ec6632205cb775
Candies <50367342+Wichen78@users.noreply.github.com>
2025-12-02 17:19:28 +0100

    Time: 0 ms (100%), Space: 54.2 MB (86.43%) - LeetHub

---
+++ LeetCode/Hard/0068-text-justification/0068-text-justification.ts
@@ -0,0 +1,40 @@
+function fullJustify(words: string[], maxWidth: number): string[] {
+    const justified = [];
+    let acc = [];
+    for (let i = 0, accLength = 0; i < words.length; i++) {
+        if (accLength + words[i].length === maxWidth) {
+            acc.push(words[i]);
+            justified.push(acc.join(' '));
+            acc = [];
+            accLength = 0;
+        } else if (accLength + words[i].length > maxWidth) {
+            justified.push(lineJustify(acc, maxWidth));
+            acc = [words[i]];
+            accLength = words[i].length + 1;
+        } else {
+            acc.push(words[i]);
+            accLength += words[i].length + 1;
+        }
+    }
+    if (acc.length > 0) {
+        const lastLine = acc.join(' ');
+        justified.push(lastLine + ' '.repeat(maxWidth - lastLine.length));
+    } else {
+        const lastLine = justified.pop().split(' ').filter(s => s.length).join(' ');
+        justified.push(lastLine + ' '.repeat(maxWidth - lastLine.length));
+    }
+    return justified;
+};
+
+function lineJustify(words: string[], maxWidth: number): string {
+    let res = words[0];
+    const length = words.length;
+    let spaces = maxWidth - words.join('').length;
+    for (let i = 1; i < length; i++) {
+        const space = Math.ceil(spaces / (length - i));
+        res += " ".repeat(space) + words[i];
+        spaces -= space;
+    }
+    
+    return res + " ".repeat(spaces);
+};
