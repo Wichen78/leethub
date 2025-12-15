89efe73cdb6ac4ff303640d953ff92e92b2076d0
Candies <50367342+Wichen78@users.noreply.github.com>
2025-12-15 17:12:43 +0100

    Time: 2 ms (56.67%), Space: 56.8 MB (55.07%) - LeetHub

--- LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
@@ -1,25 +1,14 @@
 function twoSum(numbers: number[], target: number): number[] {
-    for (let i = 0; i < numbers.length; i++) {
-        const finded = findIndex(numbers, i + 1, target - numbers[i]);
-        if (finded > 0) {
-            return [i + 1, finded + 1];
+    let left = 0, right = numbers.length - 1;
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+
+        if (sum === target) return [left + 1, right + 1];
+        if (sum > target) {
+            right--;
+        } else {
+            left++;
         }
     }
-    return [1, 2];
 };
-
-function findIndex(numbers: number[], minIndex: number, target: number): number {
-    let left = minIndex;
-    let right = numbers.length - 1;
-    do {
-        let mid = Math.ceil((left + right) / 2);
-        if (numbers[mid] === target) {
-            return mid;
-        } else if (numbers[mid] > target) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
-    } while (left <= right);
-    return 0;
-}
